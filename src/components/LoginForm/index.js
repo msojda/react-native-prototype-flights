1cbbde5fafce5c0371b643e1e1cff23ca4faa17d
Mateusz Sojda <mateusz@sojda.pl>
2017-08-02 15:00:40 +0200

    Added signing-in with Auth0.

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Container, Content, Text, Form, Item, Input, Button } from 'native-base';
+import Auth0 from 'react-native-auth0';
 
 class LoginForm extends React.Component {
-  state = { username: '', password: '' };
+  state = { username: '', password: '', error: '' };
 
-  onButtonClick() {
-    console.log(this.state);
+  constructor(props) {
+    super(props);
+
+    this.auth = new Auth0({ domain: 'msojda.eu.auth0.com', clientId: 'N0JJfL9NjbFnSwC5qqY24fNKZr2mKKzY' });
+  }
+
+  async onButtonClick() {
+    const { username, password } = this.state;
+
+    try {
+      const creds = await this.auth.auth.passwordRealm({ username, password, realm: "Username-Password-Authentication" });
+      this.setState({ username: '', password: '', error: '' });
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
   }
 
   render() {
@@ -13,29 +27,31 @@ class LoginForm extends React.Component {
       <Container>
         <Content padder>
           <Form>
-            <Item>
+            <Item error={(this.state.error.length > 0)}>
               <Input
                 placeholder="Username"
                 keyboardType='email-address'
                 autoCapitalize='none'
                 autoCorrect={false}
                 value={this.state.username}
-                onChangeText={(text) => this.setState({username: text})}
+                onChangeText={(text) => this.setState({ username: text })}
               />
             </Item>
-            <Item last>
+            <Item last error={(this.state.error.length > 0)}>
               <Input
                 placeholder="Password"
                 secureTextEntry
                 autoCapitalize='none'
                 autoCorrect={false}
                 value={this.state.password}
-                onChangeText={(text) => this.setState({password: text})}
+                onChangeText={(text) => this.setState({ password: text })}
               />
             </Item>
           </Form>
 
-          <Button style={styles.button} onPress={this.onButtonClick.bind(this)}>
+          {(this.state.error.length > 0) && <Text style={styles.error}>{this.state.error}</Text>}
+
+          <Button full style={styles.button} onPress={this.onButtonClick.bind(this)}>
             <Text>Sign in</Text>
           </Button>
         </Content>
@@ -46,7 +62,12 @@ class LoginForm extends React.Component {
 
 const styles = {
   button: {
-    margin: 20
+    marginTop: 20
+  },
+  error: {
+    color: 'red',
+    marginTop: 20,
+    fontSize: 12
   }
 };
 
